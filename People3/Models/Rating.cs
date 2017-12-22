95b88a85c3d673f03726a81cd06383736d18c157
Alexandre Konioukhov <alexan1@gmail.com>
2017-12-21 18:21:42 -0600

    rating model

---
+++ People3/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People3.Models
+{
+    public class Rating
+    {
+        public int PersonID { get; set; }
+        public string UserID { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
