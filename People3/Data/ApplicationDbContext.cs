50699b2cf3a94a3bea899c938c5aaf31b0760043
Alexandre Konioukhov <alexan1@gmail.com>
2017-12-24 23:04:42 -0600

    rating model

--- People3/Data/ApplicationDbContext.cs
+++ People3/Data/ApplicationDbContext.cs
@@ -18,10 +18,14 @@ namespace People3.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Rating>()
+        .HasKey(a => new { a.PersonID, a.UserID });
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
         public DbSet<Person> Person { get; set; }
+        public DbSet<Rating> Rating { get; set; }
     }
 }
